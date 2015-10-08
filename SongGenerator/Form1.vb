ea03b5b9b24fe5629fcab6e02c21a2450ad885d1
gcallah <gcallah@mac.com>
2015-10-08 00:42:48 -0400

    Randomization working, at least somewhat.

--- SongGenerator/Form1.vb
+++ SongGenerator/Form1.vb
@@ -4,15 +4,19 @@
     Public Property predicates As String() = {"ran over ", "shot ", "loves ", "gambled away ", "rounded up ",
         "drank up all of "}
     Public Property objects As String() = {"my heart", "some whiskey", "the flag", "a deck of cards",
-        "blue jeans", "some good stuff"}
+        "the blue jeans", "some good stuff"}
 
     Private Sub MakeSongButton_Click(sender As Object, e As EventArgs) Handles MakeSongButton.Click
         Dim result As String = ""
         Dim i As Integer = 0
-        Do While i < 3
-            result &= subjects(i)
-            result &= predicates(i)
-            result &= objects(i)
+        Do While i < 4
+            VBMath.Randomize()
+            Dim j As Integer = CInt(VBMath.Rnd() * (subjects.Length - 1))
+            result &= subjects(j)
+            j = CInt(VBMath.Rnd() * (predicates.Length - 1))
+            result &= predicates(j)
+            j = CInt(VBMath.Rnd() * (objects.Length - 1))
+            result &= objects(j)
             result &= vbNewLine
             i = i + 1
         Loop
